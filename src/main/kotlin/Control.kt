4f4328253bff8e35e86a1733dc76be4d3d6e6381
woo-chang <clllickme@naver.com>
2022-11-28 16:59:54 +0900

    제어문 관련 예제 코드

---
+++ src/main/kotlin/Control.kt
@@ -0,0 +1,41 @@
+import java.lang.IllegalArgumentException
+
+fun main() {
+
+    validateNumber(50);
+
+}
+
+fun validateNumber(number: Int) {
+    if (number < 100) {
+        throw IllegalArgumentException("${number}은 100보다 작을 수 없습니다!")
+    }
+}
+
+fun validateCase(number: Int): String {
+    return if (number > 80) {
+        "A";
+    } else if (number > 60) {
+        "B";
+    } else {
+        "F";
+    }
+}
+
+fun validateRange(number: Int): String {
+    return if (number in 80..100) {
+        "A";
+    } else {
+        "B";
+    }
+}
+
+fun validateSwitch(number: Int): String {
+    return when (number) {
+        in 91..100 -> "A"
+        90 -> "B"
+        in 80..89 -> "C"
+        60, 70, 75 -> "D"
+        else -> "F"
+    }
+}
